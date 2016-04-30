92c4d89277c2d7f4cf92097146c96f24880bad77
Matthieu Berjon <matthieu@berjon.net>
2016-04-30 22:08:23 +0200

    Rename of the /src folder to /sources
    In order to be coherent in folder names, I decided to use complete names
    such as tools, documentation, sources and so on.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

---
+++ sources/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "audio.h"
+
+#define VERSION "0.1.1"
+
+int main(int argc, char *argv[])
+{
+	PaOpenStream();
+	return EXIT_SUCCESS;
+}
---
+++ sources/tools.h
@@ -0,0 +1,7 @@
+#ifndef USEFULTOOLS
+#define USEFULTOOLS
+#define DEBUG() \
+	fprintf(stderr, "\nDEBUG\n%s:%s:%d\n\n", __FILE__, __func__, __LINE__)
+#endif
+
+unsigned int ModuloInc(const unsigned int value, const unsigned int modulus);
