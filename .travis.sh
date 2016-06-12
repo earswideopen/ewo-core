745f70fc183ce21cc0128c9f08e75e8e3a959e9c
Matthieu Berjon <matthieu@berjon.net>
2016-06-12 22:23:43 +0200

    .travis.sh: Update for packages installation
    The file has been updated to support several things:
    
    - checkinstall for a better and more robust installation under Ubuntu
    - clean of the file for a better reading.
    - syntax correction
    - update and installation of a selection of package to speed up the
      building test.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- .travis.sh
+++ .travis.sh
@@ -1,47 +1,68 @@
 #!/bin/sh
 
-PWD=$(pwd)
+ROOT_PWD=$(pwd)
 PACKAGES="\
     libtool \
     libportaudio2 \
     portaudio19-dev \
     libasound2 \
     libasound2-dev\
+    checkinstall \
     "
 
 # Install all required packages existing in Ubuntu 12.04
-sudo add-apt-repository ppa:dns/gnu -y
 sudo apt-get update -qq
 ## Specific update of autoconf and automake to avoid a bug from an obsolete command
-sudo apt-get -y install --upgrade-only autoconf automake autogen libtool 
 sudo apt-get install -y $PACKAGES
 
 # Installation of automake
-wget https://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz
-tar -xvf automake-1.15.tar.gz
-cd automake-1.15
+NAME=automake
+VERSION=1.15
+EXT=tar.gz
+wget https://ftp.gnu.org/gnu/$NAME/$NAME-$VERSION.$EXT
+tar -xvf $NAME-$VERSION.$EXT
+cd $NAME-$VERSION
 ./configure
 make
-sudo make install
+sudo checkinstall -y --pkgname $NAME --pkgversion $VERSION
+cd $ROOT_PWD
+
+# Installation of autoconf
+NAME=autoconf
+VERSION=2.69
+EXT=tar.xz
+wget https://ftp.gnu.org/gnu/$NAME/$NAME-$VERSION.$EXT
+tar -xvf $NAME-$VERSION.$EXT
+cd $NAME-$VERSION
+./configure
+make
+sudo checkinstall -y --pkgname $NAME --pkgversion $VERSION
+cd $ROOT_PWD
 
 # Installation of Cmocka framework
-wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
-tar -xvf cmocka-1.0.1.tar.xz
-cd cmocka-1.0.1
+NAME=cmocka
+VERSION=1.0.1
+EXT=tar.xz
+wget https://cmocka.org/files/1.0/$NAME-$VERSION.$EXT
+tar -xvf $NAME-$VERSION.$EXT
+cd $NAME-$VERSION
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release ..
 make
-sudo make install
-cd $PWD
+sudo checkinstall -y --pkgname $NAME --pkgversion $VERSION
+cd $ROOT_PWD
 
 # Installation of Yajl library
-wget http://github.com/lloyd/yajl/tarball/2.1.0 -O yajl-2.1.0.tar.gz
-tar -xvf yajl-2.1.0.tar.gz
+NAME=yajl
+VERSION=2.1.0
+EXT=tar.gz
+wget http://github.com/lloyd/$NAME/tarball/$VERSION -O $NAME-$VERSION.$EXT
+tar -xvf $NAME-$VERSION.$EXT
 cd lloyd-yajl-66cb08c/
 mkdir build
 cd build
 cmake ..
 make
-sudo make install
-cd $PWD
+sudo checkinstall -y --pkgname $NAME --pkgversion $VERSION
+cd $ROOT_PWD
