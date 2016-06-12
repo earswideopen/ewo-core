336a5614549f68700b5037bfa1159836bfcfabf4
Matthieu Berjon <matthieu@berjon.net>
2016-06-13 00:04:57 +0200

    .travis.yml,.travis.sh: Syntax correction
    - .travis.yml: adding of the -p argument in order to avoid to get an error
    if a build folder exists already.
    - .travis.sh: syntax error correction regarding the declaration of bash
      variables.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- .travis.sh
+++ .travis.sh
@@ -29,10 +29,10 @@ sudo apt-get update -qq
 sudo apt-get install -y $PACKAGES
 
 # Installation of automake
-NAME=${NAME:-automake}
-VERSION=${VERSION:-1.15}
-EXT=${EXT:-tar.xz}
-SUM=${SUM:-9908c75aabd49d13661d6dcb1bc382252d22cc77bf733a2d55e87f2aa2db8636}
+NAME=automake
+VERSION=1.15
+EXT=tar.xz
+SUM=9908c75aabd49d13661d6dcb1bc382252d22cc77bf733a2d55e87f2aa2db8636
 wget https://ftp.gnu.org/gnu/$NAME/$NAME-$VERSION.$EXT
 check_sig $SUM $NAME $VERSION $EXT
 tar -xvf $NAME-$VERSION.$EXT
@@ -43,10 +43,10 @@ sudo checkinstall -y --pkgname $NAME --pkgversion $VERSION
 cd $CWD
 
 # Installation of autoconf
-NAME=${NAME:-autoconf}
-VERSION=${VERSION:-2.69}
-EXT=${EXT:-tar.xz}
-SUM=${SUM:-64ebcec9f8ac5b2487125a86a7760d2591ac9e1d3dbd59489633f9de62a57684}
+NAME=autoconf
+VERSION=2.69
+EXT=tar.xz
+SUM=64ebcec9f8ac5b2487125a86a7760d2591ac9e1d3dbd59489633f9de62a57684
 wget https://ftp.gnu.org/gnu/$NAME/$NAME-$VERSION.$EXT
 check_sig $SUM $NAME $VERSION $EXT
 tar -xvf $NAME-$VERSION.$EXT
@@ -57,10 +57,10 @@ sudo checkinstall -y --pkgname $NAME --pkgversion $VERSION
 cd $CWD
 
 # Installation of Cmocka framework
-NAME=${NAME:-cmocka}
-VERSION=${VERSION:-1.0.1}
-EXT=${EXT:-tar.xz}
-SUM=${SUM:-b36050d7a1224296803d216cba1a9d4c58c31bf308b2d6d6649d61aa5a36753b}
+NAME=cmocka
+VERSION=1.0.1
+EXT=tar.xz
+SUM=b36050d7a1224296803d216cba1a9d4c58c31bf308b2d6d6649d61aa5a36753b
 wget https://cmocka.org/files/1.0/$NAME-$VERSION.$EXT
 check_sig $SUM $NAME $VERSION $EXT
 tar -xvf $NAME-$VERSION.$EXT
@@ -73,10 +73,10 @@ sudo checkinstall -y --pkgname $NAME --pkgversion $VERSION
 cd $CWD
 
 # Installation of Yajl library
-NAME=${NAME:-yajl}
-VERSION=${VERSION:-2.1.0}
-EXT=${EXT:-tar.gz}
-SUM=${SUM:-510a13e0be57cd4ba99e60ac806a3635854af51316d3131d3742a90298ccde38}
+NAME=yajl
+VERSION=2.1.0
+EXT=tar.gz
+SUM=510a13e0be57cd4ba99e60ac806a3635854af51316d3131d3742a90298ccde38
 wget http://github.com/lloyd/$NAME/tarball/$VERSION -O $NAME-$VERSION.$EXT
 check_sig $SUM $NAME $VERSION $EXT
 tar -xvf $NAME-$VERSION.$EXT
