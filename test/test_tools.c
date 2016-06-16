b9f7c320c8cd76b2cef70763ccce0e7b997fdec6
Matthieu Berjon <matthieu@berjon.net>
2016-06-16 20:02:32 +0200

    test/test_tools.c: Update of the current tests and add of a new unit test
    Update of the unit test for the IsFileExist and add a unit test for
    GetHomeDir to store the home directory.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- test/test_tools.c
+++ test/test_tools.c
@@ -2,8 +2,11 @@
 #include <stddef.h>
 #include <setjmp.h>
 #include <cmocka.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <pwd.h>
 
-#include "tools.h"
+#include "../src/tools.h"
 
 void TestModuloInc()
 {
@@ -11,10 +14,28 @@ void TestModuloInc()
 	assert_int_equal(ModuloInc(256, 256), 0);
 }
 
+void TestGetHomeDir()
+{
+	assert_non_null(GetHomeDir());
+}
+
+void TestIsFileExist()
+{
+
+	const char *file_exist = "/home/mattberjon/.ewo.json";
+	const char *file_donot_exist = "/home/mattberjon/donot_exist.json";
+
+
+	assert_int_equal(IsFileExist(file_exist), 0);
+	assert_int_equal(IsFileExist(file_donot_exist), 2);
+}
+
 int main(void)
 {
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(TestModuloInc),
+		cmocka_unit_test(TestGetHomeDir),
+		cmocka_unit_test(TestIsFileExist),
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
