fddb3fd4c42042b0b1c0582f92e6a645d21e590c
Matthieu Berjon <matthieu@berjon.net>
2017-01-12 12:50:11 +0100

    Deletion of ewo.py and its tests
    I deleted ewo.py as it was relevant in our use case. I transfered for
    archive the related tests to test_cli.py as they were refering to the
    cli testing and keep them for example later.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+test_cli
+----------------------------------
+
+Tests for `cli` module.
+"""
+
 from click.testing import CliRunner
 
 from ewo import cli
@@ -26,3 +35,39 @@ def test_cli_channel_number():
     runner = CliRunner()
     channel_result = runner.invoke(cli.main, ['--channel-number'], '2')
     assert channel_result.exit_code == 2
+
+
+#  import pytest
+#
+#  from contextlib import contextmanager
+#  from click.testing import CliRunner
+#
+#  from ewo import ewo
+#  from ewo import cli
+#
+#
+#  @pytest.fixture
+#  def response():
+#      """Sample pytest fixture.
+#      See more at: http://doc.pytest.org/en/latest/fixture.html
+#      """
+#      # import requests
+#      # return
+#      requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+#
+#
+#  def test_content(response):
+#      """Sample pytest test function with the pytest fixture as an argument.
+#      """
+#      # from bs4 import BeautifulSoup
+#      # assert 'GitHub' in BeautifulSoup(response.content).title.string
+#
+#
+#  #  def test_command_line_interface():
+#  #      runner = CliRunner()
+#  #      result = runner.invoke(cli.main)
+#  #      assert result.exit_code == 0
+#  #      assert 'ewo.cli.main' in result.output
+#  #      help_result = runner.invoke(cli.main, ['--version'])
+#  #      assert help_result.exit_code == 0
+#      assert 'ewo-core 0.2.0 (Python 3.5)' in help_result.output
